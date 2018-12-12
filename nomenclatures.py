e16e1b799795bbd906e3ec36485a4ce99e1bab35
robincoello <robincoello@hotmail.com>
2018-12-12 15:44:34 +0100

    mep delet code basur

--- nomenclatures.py
+++ nomenclatures.py
@@ -24,58 +24,13 @@ password = '1111'
 import xmlrpclib
 import os
 import json
-
 import getpass
-
 import pandas 
 
 
 
 print "Your password odoo (" + url + ")"
 password = getpass.getpass()
-
-table = "mrp.bom"
-
-def rc_campos_bom():
-    return {
-            'fields': [
-            'id',
-            'code',
-            'active',
-            'type',
-            'product_tmpl_id',
-            'product_id',
-            'product_qty',
-            'product_uom_id',
-            'sequence',
-            'routing_id',
-            'ready_to_produce',
-            'picking_type_id',
-            'company_id',
-            'message_last_post',
-            'create_uid',
-            'create_date',
-            'write_uid',
-            'write_date'
-                        ]
-        }
-def rc_campos_bom_line():
-    return {
-            'fields': [
-            'id',
-            'product_id',
-            'product_qty',
-            'product_uom_id',
-            'sequence',
-            'routing_id',
-            'bom_id',
-            'operation_id',            
-            'create_uid',
-            'create_date',
-            'write_uid',
-            'write_date'
-                        ]
-        }
 
 
 common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
@@ -99,11 +54,6 @@ models.execute_kw(
     'check_access_rights',
     ['read'], {'raise_exception': False})
 
-
-def total_products():
-    return models.execute_kw(db, uid, password,
-    'res.partner', 'search_count',
-    [[['is_company', '=', True], ['customer', '=', True]]])
 
 
 
@@ -134,9 +84,7 @@ def detalles(rc_id):
     total = 0
     for reg in resultat:
         total += 1
-    
-            
-    #print "Customers list : "+str(resultat)
+                
 
     print(json.dumps(resultat, indent=4))
 
@@ -187,23 +135,6 @@ def list():
     print("Total de nomenclatures: " + repr(total))
 
 ##############################################
-##############################################
-def search(rc_name):
-    print("**************************************")
-    print("Search for : " + rc_name )
-    #rc_id_customer = raw_input("Customer number please ")
-
-    ids = models.execute_kw(db, uid, password,
-    'product.product', 'search',
-    [[ ['name','=',rc_name]]],
-    )
-
-    resultat = models.execute_kw(db, uid, password, table, 'read',[ids], rc_campos()
-
-    )
-   # print "Customers list : "+str(resultat)
-    print(json.dumps(resultat, indent=4))
-
 def jsontoxls():    
     pandas.read_json("odoo.json").to_excel("odoo.xlsx")
 
@@ -244,18 +175,6 @@ while(opcion > 6 or opcion < 1 ):
     print("Please a number between 1 - 6")
     opcion = input()
 
-
-if opcion == 111:
-    print("**************************************")
-
-    resultat = models.execute_kw(db, uid, password,
-    'res.partner', 'search',
-    [[
-        ['customer', '=', True],
-        
-    ]])
-    print "List : "+str(resultat)
-
 ####################################################
 ######### L I S T ##################################
 #
@@ -275,50 +194,5 @@ if opcion == 3:
     rc_id = raw_input("Nomenclature ID number please ")
 ## D E T A I L S
     detalles(rc_id)
-   
 
-
-
-
-####################################################
-######### D E L E T E ##############################
-
-if opcion == 5:
-    print("**************************************")
-    print("Delete")
-
-    rc_id = raw_input("Id to delete please: ")
-    id = int(rc_id)
-    models.execute_kw(db, uid, password, 'product.product', 'unlink', [[id]])
-    # check if the deleted record is still in the database
-
-    verif = models.execute_kw(db, uid, password,
-        'product.product', 'search', [[['id', '=', id]]])
-    if not verif:
-        print('The item is delete for ever ! ')
-
-    detalles(id)
-####################################################
-######### D E L E T E ##############################
-if opcion == 10:
-    print("**************************************")
-
-    resultat = models.execute_kw(db, uid, password,
-    'res.partner', 'search',
-    [[
-        ['customer', '=', True],
-        ['is_company', '=', True],
-    ]])
-    print "Companies list : "+str(resultat)
-
-
-####################################################
-######### S E A R C H  ###########################
-
-if opcion == 6:
-    print("**************************************")
-    print("Search")
-    rc_name = raw_input("Name please ")
-## D E T A I L S
-    search(rc_name)
     
